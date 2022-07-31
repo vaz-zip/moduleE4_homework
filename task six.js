730ef2830994c1a627dafa021f70032788cd332b
KONSNANTIN SHIRYAEV <97960873+vaz-zip@users.noreply.github.com>
2022-07-31 19:26:41 +0400

    Add files via upload

---
+++ task six.js
@@ -0,0 +1,13 @@
+let arr = [3, 3, 3, 3]
+
+let result = true;
+let index = arr[0];
+
+for (let i of arr) {
+    if (i !== index) {
+        result = false;
+    }
+}
+console.log(arr)
+console.log(index)
+console.log(`Мяушка: ${result}`)
