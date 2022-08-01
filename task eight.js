65ddfc37c5c6d66c850433bcd701bbfca524e016
KONSNANTIN SHIRYAEV <97960873+vaz-zip@users.noreply.github.com>
2022-08-01 18:38:18 +0400

    Delete task eight.js

--- task eight.js
+++
@@ -1,9 +0,0 @@
-let map = new Map();
-map.set("apple", "green");
-map.set("strawberry", "red");
-map.set("blueberry", "blue")
-
-let arr = map.keys()
-for (let key of arr) {
-    console.log(`"Ключ-${key}, значение-${map.get(key)}"`)
-}
