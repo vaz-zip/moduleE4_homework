ee4a7f7818848f7435741c64ce1aa8f1b1fe086d
KONSNANTIN SHIRYAEV <97960873+vaz-zip@users.noreply.github.com>
2022-08-01 13:17:59 +0400

    Add files via upload

---
+++ task_E4.4.js
@@ -0,0 +1,57 @@
+function ElectricalDevice(power, name, activation) {
+    this.name = name;
+    this.voltage = '220 вольт';
+    //this.power = power;
+    this.activation = activation;
+    this.getSwitch = function (activation) {
+        console.log(this.name + ' работает от сети ' + this.voltage + ' и включается ' + activation)
+    }
+
+}
+
+function LightingDevices(name, lightPower, power, activation) {
+    this.name = name;
+    this.light_power = lightPower;
+    this.power = power;
+    this.activation = activation;
+    this.getLamp = function () {
+        console.log(`Световой мощности этой лампы достаточно для освещения ${Math.floor(lightPower / 150)} кв.м жилой комнаты `)
+    }
+}
+
+LightingDevices.prototype = new ElectricalDevice()
+
+function digitalTechnology(name, power, activation, memorySize){
+    this.name = name;
+    this.power = power;
+    this.activation = activation;
+    this.memorySize = memorySize;
+    this.getComp = function(){
+        console.log(`Оперативка у компа ${memorySize}!`)
+    }
+}
+
+digitalTechnology.prototype = new ElectricalDevice()
+
+function climateEquipment(name, power, activation, BTU){
+    this.name = name;
+    this.power = power;
+    this.activation = activation;
+    this.getFreeze = function(){
+        console.log(`Мощность охлаждения данного кондиционера - BTU позволяет поддерживать комфортную температуру в помещении ${(BTU / 3412) * 10} кв.м`)
+    }
+}
+climateEquipment.prototype = new ElectricalDevice()
+
+const tableLamp = new LightingDevices('Настольная лампа', 600, 9, 'вручную');
+const computer = new digitalTechnology('Компьютер', 400, 'вручную', '1 Тб');
+const conditioner = new climateEquipment('Кондиционер', 2500, 'ПУ', 8530);
+
+
+
+tableLamp.getSwitch('вручную!')
+computer.getSwitch('вручную!')
+conditioner.getSwitch('ПУ!')
+tableLamp.getLamp()
+computer.getComp()
+conditioner.getFreeze()
