d0ee86a50696e7b0c205ccc4190e9f5b78feb94f
KONSNANTIN SHIRYAEV <97960873+vaz-zip@users.noreply.github.com>
2022-08-01 17:09:13 +0400

    Update task_E4.4.js

--- task_E4.4.js
+++ task_E4.4.js
@@ -1,13 +1,25 @@
-function ElectricalDevice(power, name, activation) {
+function ElectricalDevice(power, name, activation, deviceSwitch, deviceTurned) {
     this.name = name;
     this.voltage = '220 вольт';
-    //this.power = power;
+    this.deviceTurned = false;
+    this.power = power;
     this.activation = activation;
+    this.deviceSwitch = function (deviceSwitch) {
+        if (deviceSwitch === 'on') {
+            this.deviceTurned = true;
+        } else {
+            this.deviceTurned = false;
+        }
+        console.log(`Электроприбор включён: ${this.deviceTurned}`)
+    }
     this.getSwitch = function (activation) {
         console.log(this.name + ' работает от сети ' + this.voltage + ' и включается ' + activation)
+
     }
 
 }
+
+
 
 function LightingDevices(name, lightPower, power, activation) {
     this.name = name;
@@ -21,32 +33,32 @@ function LightingDevices(name, lightPower, power, activation) {
 
 LightingDevices.prototype = new ElectricalDevice()
 
-function digitalTechnology(name, power, activation, memorySize){
+function digitalTechnology(name, power, activation, memorySize) {
     this.name = name;
     this.power = power;
     this.activation = activation;
     this.memorySize = memorySize;
-    this.getComp = function(){
+    this.getComp = function () {
         console.log(`Оперативка у компа ${memorySize}!`)
     }
 }
 
 digitalTechnology.prototype = new ElectricalDevice()
 
-function climateEquipment(name, power, activation, BTU){
+function climateEquipment(name, power, activation, BTU) {
     this.name = name;
     this.power = power;
     this.activation = activation;
-    this.getFreeze = function(){
+    this.getFreeze = function () {
         console.log(`Мощность охлаждения данного кондиционера - BTU позволяет поддерживать комфортную температуру в помещении ${(BTU / 3412) * 10} кв.м`)
     }
 }
+
 climateEquipment.prototype = new ElectricalDevice()
 
 const tableLamp = new LightingDevices('Настольная лампа', 600, 9, 'вручную');
 const computer = new digitalTechnology('Компьютер', 400, 'вручную', '1 Тб');
 const conditioner = new climateEquipment('Кондиционер', 2500, 'ПУ', 8530);
-
 
 
 tableLamp.getSwitch('вручную!')
@@ -55,3 +67,4 @@ conditioner.getSwitch('ПУ!')
 tableLamp.getLamp()
 computer.getComp()
 conditioner.getFreeze()
+tableLamp.deviceSwitch('on'); // Включаем прибор ('on') или выключаем (любое значение, отличное от 'on')
